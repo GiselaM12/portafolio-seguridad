b39faa532e6ca989351712b050fd3d7583d6c5f2
Gisela Moreno <solisgisela479@gmail.com>
2026-02-18 16:30:01 -0600

    Fix 404 error using HashRouter and setting correct base path

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
 import Activities from './pages/Activities'
@@ -7,7 +7,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="min-h-screen bg-cyber-dark text-gray-100 font-sans">
         <Header />
         <main>
@@ -19,7 +19,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portafolio-seguridad/',
   base: '/portafolio-seguridad/',
 })
